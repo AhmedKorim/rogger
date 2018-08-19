4c01c81eaf865a1c19d9f35b21a6b5ee8eca3a9d
ahmedkorim <ahmedkorrim@gmail.com>
2018-08-19 15:35:30 +0300

    added bootstrap container

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -2,31 +2,61 @@ import React from 'react'
 import Toolbar from "@material-ui/core/Toolbar/Toolbar";
 import AppBar from "@material-ui/core/AppBar/AppBar";
 import Grid from "@material-ui/core/Grid/Grid";
+import {withStyles} from '@material-ui/core'
 
-const styles = theme => ({})
+const styles = theme => {
+    console.log(theme);
+    return ({
+
+        mini: {
+            maxHeight: 30,
+            minHeight: 30,
+            boxShadow: theme.shadows[5],
+            position:'relative',
+            zIndex:2
+
+        },
+        grid: {
+            [theme.breakpoints.down('md')]: {
+                margin: '5px 0'
+            }
+        },
+        shadow: {
+            boxShadow: theme.shadows[5]
+        },
+        richToolbar: {
+            backgroundColor: theme.palette.background.default
+        }
+
+    });
+}
 const Header = props => {
-
+    const {
+        classes
+    } = props;
     return (
         <header>
-            <AppBar position="static">
-                <Toolbar variant="dense">
+            <AppBar position="fixed" className={[classes.shadow].join(' ')}>
+                <Toolbar variant="dense" className={classes.mini}>
                     <Grid container>
-                        <Grid item xs={6} md>
+                        <Grid item xs className={classes.grid}>
                             currency
                         </Grid>
-                        <Grid item xs={6} md>
+                        <Grid item xs className={classes.grid}>
                             language
                         </Grid>
-                        <Grid item xs={6} md>
+                        <Grid item xs className={classes.grid}>
                             account
                         </Grid>
-                        <Grid item xs={6} md>
+                        <Grid item xs className={classes.grid}>
                             location
                         </Grid>
                     </Grid>
+                </Toolbar>
+                <Toolbar variant="regular" className={classes.richToolbar}>
                 </Toolbar>
             </AppBar>
         </header>
     )
 }
-export default Header
+export default withStyles(styles)(Header);
