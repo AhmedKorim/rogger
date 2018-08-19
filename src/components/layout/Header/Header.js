123d2bb98bdd661f4c2f48fbb88a9ff5d24041b1
ahmedkorim <ahmedkorrim@gmail.com>
2018-08-19 16:03:58 +0300

    adedd search field

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -2,7 +2,10 @@ import React from 'react'
 import Toolbar from "@material-ui/core/Toolbar/Toolbar";
 import AppBar from "@material-ui/core/AppBar/AppBar";
 import Grid from "@material-ui/core/Grid/Grid";
-import {withStyles} from '@material-ui/core'
+import {withStyles, withWidth} from '@material-ui/core'
+import Typography from "@material-ui/core/Typography/Typography";
+import TextField from "@material-ui/core/TextField/TextField";
+import Icon from "@material-ui/core/Icon/Icon";
 
 const styles = theme => {
     console.log(theme);
@@ -12,8 +15,8 @@ const styles = theme => {
             maxHeight: 30,
             minHeight: 30,
             boxShadow: theme.shadows[5],
-            position:'relative',
-            zIndex:2
+            position: 'relative',
+            zIndex: 2
 
         },
         grid: {
@@ -26,37 +29,68 @@ const styles = theme => {
         },
         richToolbar: {
             backgroundColor: theme.palette.background.default
+        },
+        flex: {
+            justifyContent: "center",
+            alignItems: "baseline"
         }
 
     });
 }
 const Header = props => {
     const {
-        classes
+        classes,
+        width
     } = props;
+    // if width is lower than md show hamburger menu
     return (
         <header>
             <AppBar position="fixed" className={[classes.shadow].join(' ')}>
                 <Toolbar variant="dense" className={classes.mini}>
-                    <Grid container>
-                        <Grid item xs className={classes.grid}>
-                            currency
+                    <div className="container">
+                        <Grid container>
+                            <Grid item xs className={classes.grid}>
+                                currency
+                            </Grid>
+                            <Grid item xs className={classes.grid}>
+                                language
+                            </Grid>
+                            <Grid item xs className={classes.grid}>
+                                account
+                            </Grid>
+                            <Grid item xs className={classes.grid}>
+                                location
+                            </Grid>
                         </Grid>
-                        <Grid item xs className={classes.grid}>
-                            language
-                        </Grid>
-                        <Grid item xs className={classes.grid}>
-                            account
-                        </Grid>
-                        <Grid item xs className={classes.grid}>
-                            location
-                        </Grid>
-                    </Grid>
+                    </div>
                 </Toolbar>
                 <Toolbar variant="regular" className={classes.richToolbar}>
+                    <div className="container">
+                        <Grid container className={classes.flex}>
+                            <Grid item xs={3} md={2}>
+                                <Typography variant="headline">
+                                    Rogger
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={9} md>
+                                <div>
+                                    <Grid container spacing={8} alignItems="flex-end" >
+                                        <Grid item>
+                                            <Icon color="primary">search</Icon>
+                                        </Grid>
+                                        <Grid item>
+                                            <TextField id="input-with-icon-grid" label="search evey thing"/>
+                                        </Grid>
+                                    </Grid>
+                                </div>
+                            </Grid>
+                            <Grid item md>
+                            </Grid>
+                        </Grid>
+                    </div>
                 </Toolbar>
             </AppBar>
         </header>
     )
 }
-export default withStyles(styles)(Header);
+export default withWidth()(withStyles(styles)(Header));
