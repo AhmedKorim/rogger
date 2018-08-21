7d2898468d68e610771bc29fda4b15aa0d2bf874
ahmedkorim <ahmedkorrim@gmail.com>
2018-08-21 13:36:27 +0300

    addedd breadcrumb

--- src/components/UI/Breadcrumbs/breadcrumbs.js
+++ src/components/UI/Breadcrumbs/breadcrumbs.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {withStyles} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper/Paper";
+import "./Breadcrumbs.scss"
+import Icon from "@material-ui/core/es/Icon/Icon";
+import Button from "@material-ui/core/es/Button/Button";
+import Typography from "@material-ui/core/Typography/Typography";
+import {withRouter} from "react-router-dom";
 
 const styles = theme => ({
-    paper: theme.palette.background.default
+    paper: {
+        backgroundColor: theme.palette.background.default
+    }
 })
-const breadcrumbs = props => {
-    const {classes} = props;
+const Breadcrumbs = props => {
+    const {
+        classes,
+        location: {pathname}
+    } = props;
+    const pathes = pathname.split('/').filter(item => item.length > 0);
+    console.log(pathes);
     return (
-        <div className="container">
-            <Paper className={classes.paper}>
-
+            <Paper className={[classes.paper, 'breadcrumbs']}>
+                <div>
+                    <Button
+                        className="breadcrumbsButton"
+                        size="small">
+                        <Icon>home</Icon>
+                        <Typography className="breadcrumbsLink">home</Typography>
+                    </Button>
+                </div>
+                <div className="chevronWrapper">
+                    <Icon>chevron_right</Icon>
+                </div>
+                {pathes.map((path, index) => <Fragment>
+                    <div>
+                        <Button
+                            className="breadcrumbsButton"
+                            size="small">
+                            <Typography className="breadcrumbsLink">{path}</Typography>
+                        </Button>
+                    </div>
+                    {index === pathes.length - 1 ? null : <div className="chevronWrapper">
+                        <Icon>chevron_right</Icon>
+                    </div>}
+                </Fragment>)}
             </Paper>
-        </div>
     )
 }
-export default withStyles(styles)(breadcrumbs);
+export default withRouter(withStyles(styles)(Breadcrumbs));
--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import withPadding from "../../HOC/WithPadding/WithPadding";
+import Breadcrumbs from "../../components/UI/Breadcrumbs/breadcrumbs";
 
 class Products extends React.Component {
     render() {
         return (
             <div className="products">
-
+                <Breadcrumbs/>
             </div>
         )
     }
