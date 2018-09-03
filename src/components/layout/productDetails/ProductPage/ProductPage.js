60fd3c4321c73c1b8a322dc21158f41792d7e566
ahmedkorim <ahmedkorrim@gmail.com>
2018-09-03 16:06:11 +0300

    fix scroll bar with main tag

--- src/components/layout/productDetails/ProductPage/ProductPage.js
+++ src/components/layout/productDetails/ProductPage/ProductPage.js
@@ -12,6 +12,7 @@ import EnhancedTabs from "../../../UI/EnhancedTabs/EnhancedTabs";
 import ProductDetails from "../ProductDetails";
 import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
+import PerfectScrollbar from 'react-perfect-scrollbar'
 
 
 const styles = theme => ({
@@ -45,40 +46,45 @@ class ProductPage extends React.Component {
                 {product && <Fragment>
                     <Breadcrumbs/>
                     <Container>
-                        <Grid container alignItems="flex-start">
-                            <Grid item container xs lg={10}>
-                                <ProductDetails product={product}/>
-                                <Grid container>
-                                    <Grid xs>
-                                        <Card className={classes.root}>
-                                            <EnhancedTabs
-                                                value={0}
-                                                tab={[
-                                                    {label: 'about'},
-                                                    {label: 'reviews'},
-                                                    {label: 'FQA'}
-                                                ]}
-                                            >
-                                                <div>
-                                                    <Typography variant="subheading">About</Typography>
-                                                </div>
-                                                <div>
-                                                    <Typography variant="subheading">reviews</Typography>
-                                                </div>
-                                                <div>
-                                                    <Typography variant="subheading">FQA</Typography>
-                                                </div>
-                                            </EnhancedTabs>
-                                        </Card>
+                            <Grid container alignItems="flex-start">
+                                <Grid item container xs lg={10}>
+                                    <ProductDetails product={product}/>
+                                    <Grid container>
+                                        <Grid xs>
+                                            <Card className={classes.root}>
+                                                <EnhancedTabs
+                                                    value={0}
+                                                    tab={[
+                                                        {label: 'about'},
+                                                        {label: 'reviews'},
+                                                        {label: 'FQA'}
+                                                    ]}
+                                                >
+                                                    <div>
+                                                        <Typography variant="subheading">About</Typography>
+                                                        <br/>
+                                                        <br/>
+                                                        <br/>
+                                                        <br/>
+                                                        <br/>
+                                                    </div>
+                                                    <div>
+                                                        <Typography variant="subheading">reviews</Typography>
+                                                    </div>
+                                                    <div>
+                                                        <Typography variant="subheading">FQA</Typography>
+                                                    </div>
+                                                </EnhancedTabs>
+                                            </Card>
+                                        </Grid>
                                     </Grid>
                                 </Grid>
+                                {(width === 'lg' || width === 'xl') ? (
+                                    <Grid item lg className={classes.pannerCont}>
+                                        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i, index) => <div key={i * index + Math.random().toFixed(2)}><Panner/></div>)}
+                                    </Grid>
+                                ) : null}
                             </Grid>
-                            {(width === 'lg' || width === 'xl') ? (
-                                <Grid item lg className={classes.pannerCont}>
-                                    {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i, index) => <div key={i * index + Math.random().toFixed(2)}><Panner/></div>)}
-                                </Grid>
-                            ) : null}
-                        </Grid>
                     </Container>
                 </Fragment>}
             </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,6 @@ import Header from "../components/layout/Header/Header";
 import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import Home from "./Home/Home";
 import RDialog from '../components/UI/Dialog/Dialog'
-import PerfectScrollbar from 'react-perfect-scrollbar'
 import {connect} from "react-redux";
 import Products from "./Products/Products";
 import ProductPage from "../components/layout/productDetails/ProductPage/ProductPage";
@@ -12,6 +11,8 @@ import {getData} from "../dux/actions/productsActions";
 import AuthDialogOpener from "./Auth/AuthDialogOpener";
 import AdminDashboard from "./Admin/Dashboard/AdminDashboard";
 import UserDashboard from "./User/Dashboard/UserDashboard";
+import PerfectScrollbar from "react-perfect-scrollbar";
+import Container from "../HOC/Container/Container";
 
 
 class App extends Component {
@@ -26,9 +27,9 @@ class App extends Component {
         return (
             <Fragment>
                 <Header/>
-                <PerfectScrollbar>
-                    <Route path={this.props.location.pathname.substring(0, this.props.location.pathname.length - 4) + 'auth'} component={AuthDialogOpener}/>
-                    <main style={{height: '100vh'}}>
+                <Route path={this.props.location.pathname.substring(0, this.props.location.pathname.length - 4) + 'auth'} component={AuthDialogOpener}/>
+                <main style={{height: '100vh'}}>
+                    <PerfectScrollbar>
                         <Switch>
                             <Route path="/products/:productId" component={ProductPage}/>
                             <Route path="/products" component={Products}/>
@@ -39,8 +40,8 @@ class App extends Component {
                             <Route path="/dashboard" component={AdminDashboard}/>
                             <Route path="/my_dashboard" component={UserDashboard}/>
                         </Switch>
-                    </main>
-                </PerfectScrollbar>
+                    </PerfectScrollbar>
+                </main>
                 <RDialog/>
             </Fragment>
         )
